7227bf8eb6531f4455ea4d4403294b1ab89613f8
BijnensBram <bram@bijnens.com>
2020-10-12 11:02:42 +0200

    minor updates

--- pulse/flip.cpp
+++ pulse/flip.cpp
@@ -1,4 +1,5 @@
 #include "flip.h"
+#include <cmath>
 
 /* function to initialise sigma */
 /* in 1d only two directions are possible for movement left or rigth => c = +/- 1 */
@@ -17,3 +18,16 @@ double fliptest(double a, double dt, double rand){
 		return 1;
 	}
 }
+
+double fliptest_a_peaks(double x, double a,double l, double dt, double rand){
+	if (rand < a_profile_peaks(x,a,l)*dt){
+		return -1;
+	} else {
+		return 1;
+	}
+}
+
+double a_profile_peaks(double x,double a,double l){
+	return a*(exp(-500*x*x)+exp(-500*(x-l*0.25)*(x-l*0.25))+exp(-500*(x-l*0.5)*(x-l*0.5))+exp(-500*(x-l*0.75)*(x-l*0.75))+exp(-500*(x-l)*(x-l)));
+}
+
--- pulse/flip.h
+++ pulse/flip.h
@@ -7,4 +7,10 @@ double initsigma(double rand);
 /* function to check of spin flips */
 double fliptest(double a, double dt, double rand);
 
+/* function to check of spin flips using a peaked a profile*/
+double fliptest_a_peaks(double x, double a, double l, double dt, double rand);
+
+/* function to create a profile */
+double a_profile_peaks(double x, double a, double l);
+
 #endif //FUNCTIONS
